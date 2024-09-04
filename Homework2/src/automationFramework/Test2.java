c6eff91dee1ee5a0377531f9808966a3372fedd1
Fatima Hani Abbadi <78602738+fatima-abbadi@users.noreply.github.com>
2024-09-04 10:44:05 +0300

    Update Test2.java

--- Homework2/src/automationFramework/Test2.java
+++ Homework2/src/automationFramework/Test2.java
@@ -24,7 +24,7 @@ public class Test2 {
 
     @BeforeClass
     public void setUp() {
-        // Set up ChromeDriver path
+     
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\ProBook\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe"); 
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
@@ -33,10 +33,10 @@ public class Test2 {
 
     @DataProvider(name = "data")
     public Object[][] loginDataProvider() throws IOException {
-        // Excel file path
+        
         String filePath = "C:\\Users\\ProBook\\eclipse-workspace\\Homework2\\src\\automationFramework\\data.xlsx";
         FileInputStream file = new FileInputStream(new File(filePath));
-        // Check if file exists
+       
         File file1 = new File(filePath);
         if (!file1.exists()) {
             throw new IOException("Excel file not found at path: " + filePath);
@@ -53,7 +53,7 @@ public class Test2 {
             data[i - 1][0] = row.getCell(0).getStringCellValue(); 
             data[i - 1][1] = row.getCell(1).getStringCellValue(); 
 
-            // Define the expected message for each row
+           
             if (i == 1) {
                 data[i - 1][2] = "Invalid email format, password empty.";
             } else if (i == 2) {
@@ -71,7 +71,7 @@ public class Test2 {
        
         driver.get("https://www.frontgate.com/ShoppingCartView");
 
-        // Enter username and password
+        
         WebElement usernameField = driver.findElement(By.id("gwt-sign-in-modal"));
         WebElement passwordField = driver.findElement(By.id("passwordReset")); 
         WebElement loginButton = driver.findElement(By.id("logonButton")); 
@@ -82,17 +82,17 @@ public class Test2 {
         passwordField.sendKeys(password);
         loginButton.click();
 
-        // Get the actual message displayed
-        WebElement messageElement = driver.findElement(By.id("messageElement")); // Replace with the actual ID of the message element
+       
+        WebElement messageElement = driver.findElement(By.id("messageElement")); 
         String actualMessage = messageElement.getText();
 
-        // Assert the expected and actual messages
+
         Assert.assertEquals(actualMessage, expectedMessage);
     }
 
     @AfterClass
     public void tearDown() {
-        // Close the browser
+      
         if (driver != null) {
             driver.quit();
         }
